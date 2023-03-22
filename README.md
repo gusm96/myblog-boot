52245183764e56e46d5927de33a50d2e6a198b1f
Seongmo Gu <77833389+gusm96@users.noreply.github.com>
2023-03-22 16:31:52 +0900

    Upload README.md File

---
+++ README.md
@@ -0,0 +1,15 @@
+# Project - MyBlog 
+
+### (Spring boot version)
+
+------
+
+### 프로젝트를 시작한 계기
+
+- Spring Boot 와 JPA 를 경험하고자 기존 Myblog 프로젝트를 중단하고 새로 시작하게 되었습니다.
+- MariaDB를 사용해보고 싶었습니다.
+- TDD(Test-Driven-Developer)방법론을 적용하여 개발해보고 싶었습니다.
+- 개인 블로그를 직접 개발하고 배포하여 유지보수하며 운영해보고 싶었습니다.
+
+------
+
---
+++ src/test/java/com/moya/myblogboot/service/BoardServiceTest.java
@@ -0,0 +1,28 @@
+package com.moya.myblogboot.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardServiceTest {
+
+    @Test
+    void getBoardList() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void editBoard() {
+    }
+
+    @Test
+    void deleteBoard() {
+    }
+
+    @Test
+    void newPost() {
+    }
+}
