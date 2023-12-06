5cbbf626cc80e89298a11027a3023c3cb822f4db
Seongmo Gu <gusm96@naver.com>
2023-12-06 23:06:29 +0900

    Spring security 수정

--- src/main/java/com/moya/myblogboot/configuration/WebSecurityConfig.java
+++ src/main/java/com/moya/myblogboot/configuration/WebSecurityConfig.java
@@ -5,8 +5,10 @@ import com.moya.myblogboot.service.implementation.AuthServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,18 +23,17 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig  {
     @Value("${jwt.secret}")
     private String secret;
     private AuthService authService;
-
     public void setAuthService(AuthService authService) {
         this.authService = authService;
     }
 
-    public AuthService getAuthService() {
+  /*  public AuthService getAuthService() {
         return this.authService;
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -40,9 +41,18 @@ public class WebSecurityConfig {
                 .csrf().disable() // Cross Site Request Forgery (사이트간 위조요청) Non-browser clients service에선 disable 가능 (Spring security 에선 기본 설정이 protection)
                 .cors().and() // Cross Origin Resource Sharing (교차 출처 리소스 공유)
                 .authorizeHttpRequests()
-                .requestMatchers(new AntPathRequestMatcher("/api/v1/management/**")).hasRole("ADMIN")
-                .requestMatchers(new AntPathRequestMatcher("/api/v1/comment/**")).hasAnyRole("NORMAL", "ADMIN")
-                .requestMatchers(new AntPathRequestMatcher("/api/v1/likes/**")).hasAnyRole("NORMAL", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/v1/boards").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/v1/boards/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/boards/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/v1/comment/**").hasAnyRole("NORMAL", "ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/v1/comment/**").hasAnyRole("NORMAL", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/comment/**").hasAnyRole("NORMAL", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/v1/categories").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/v1/categories/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/categories/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/v1/likes/**").hasAnyRole("NORMAL", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/v1/likes/**").hasAnyRole("NORMAL", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE,"/api/v1/likes/**").hasAnyRole("NORMAL", "ADMIN")
                 .requestMatchers(new AntPathRequestMatcher("/api/v1/images/**")).hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
