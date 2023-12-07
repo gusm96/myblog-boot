533d02a4eeaa4834ab2cfa01985ff6e5a7bba266
Seongmo Gu <gusm96@naver.com>
2023-12-08 04:47:40 +0900

    서버 설정 수정

--- src/main/java/com/moya/myblogboot/MyblogBootApplication.java
+++ src/main/java/com/moya/myblogboot/MyblogBootApplication.java
@@ -3,9 +3,10 @@ package com.moya.myblogboot;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-@RequiredArgsConstructor
 public class MyblogBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyblogBootApplication.class, args);
