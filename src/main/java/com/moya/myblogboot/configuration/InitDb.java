9d88e41380ed94a7ee78e7ca8587a4cfaca8aa7a
Seongmo Gu <gusm96@naver.com>
2023-12-10 23:21:53 +0900

    Webhook Test

--- src/main/java/com/moya/myblogboot/configuration/InitDb.java
+++ src/main/java/com/moya/myblogboot/configuration/InitDb.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+@Component
 @RequiredArgsConstructor
 public class InitDb {
     private final InitService initService;
@@ -29,6 +30,7 @@ public class InitDb {
             initService.initBoard(adminMember, category2, "파이썬", "파이썬 진짜 멋지네~~");
         }
     }
+    @Component
     @RequiredArgsConstructor
     static class InitService {
         private final EntityManager em;
