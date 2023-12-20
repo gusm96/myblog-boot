42c26669f52b42140c6178d7c39e8b168a61adf9
Seongmo Gu <gusm96@naver.com>
2023-12-20 23:04:59 +0900

    Entity 생성/수정/삭제일 추상클래스 추가

---
+++ src/main/java/com/moya/myblogboot/domain/base/BaseTimeEntity.java
@@ -0,0 +1,36 @@
+package com.moya.myblogboot.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    private LocalDateTime updateDate;
+
+    private LocalDateTime deleteDate;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createDate = now;
+        updateDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateDate = LocalDateTime.now();
+    }
+
+    public void delete(){
+        this.deleteDate = LocalDateTime.now();
+}}
