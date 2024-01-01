31938a6f68358fbf3921cea822a96c87f86dc517
Seongmo Gu <gusm96@naver.com>
2024-01-01 18:15:48 +0900

    화면 업데이트

--- src/main/frontend/src/components/Comments/Comment.js
+++ src/main/frontend/src/components/Comments/Comment.js
@@ -1,106 +1,107 @@
-import axios from "axios";
 import React, { useState } from "react";
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-  ModalTitle,
-} from "react-bootstrap";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import { addComment, getChildComments } from "../../services/boardApi";
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/userSlice";
-import { addComment } from "../../services/boardApi";
+import { selectAccessToken } from "../../redux/userSlice";
+import { formatTimeAgo } from "../dateFormat";
 
-export const Comment = ({ boardId, accessToken }) => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const [isLoggedInModal, setIsLoggedInModal] = useState(false);
-  const [commentData, setCommentData] = useState({
+export const Comment = ({ boardId, comment }) => {
+  const accessToken = useSelector(selectAccessToken);
+  const [reply, setReply] = useState({
     comment: "",
-    parentId: "",
+    parentId: comment.id,
   });
+  const [showReply, setShowReply] = useState(false);
+  const [showReplyForm, setShowReplyForm] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setCommentData((prevData) => ({
-      ...prevData,
-      [name]: value,
+  const [child, setChild] = useState([]);
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    setReply((prevReply) => ({
+      ...prevReply,
+      comment: e.target.value,
     }));
   };
 
-  const handleCommentBoxClick = (e) => {
-    e.preventDefault();
-    if (!isLoggedIn) {
-      setIsLoggedInModal(true);
-    }
+  const handleShowChild = (parentId) => {
+    getChildComments(parentId)
+      .then((data) => {
+        setChild(data);
+        setShowReply(true);
+      })
+      .catch((error) => console.log(error));
   };
-
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    // textarea의 값이 0 or null or " "이 아닌지 확인
-    if (commentData.comment.trim() === "") {
-      alert("댓글의 내용을 입력하세요.");
-    } else {
-      addComment(boardId, commentData, accessToken)
-        .then((data) => {
-          window.location.reload();
-        })
-        .catch((error) => console.log(error));
-    }
-  };
-
-  const handleCloseLoginModal = () => {
-    setIsLoggedInModal(false);
+    // axios
+    addComment(boardId, reply, accessToken)
+      .then((data) => {
+        alert(data);
+        window.location.reload();
+      })
+      .catch((error) => console.log(error));
   };
   return (
-    <form
-      onSubmit={handleOnSubmit}
-      style={{
-        display: "flex",
-      }}
-    >
-      <textarea
-        onClick={handleCommentBoxClick}
-        name="comment"
-        placeholder="댓글을 입력하세요."
-        value={commentData.comment}
-        onChange={handleChange}
-        style={{
-          width: "70%",
-          margin: "0 10px 0 0", // 수정된 부분
-          padding: "10px",
-          borderRadius: "5px",
-          border: "1px solid #ced4da",
-        }}
-      ></textarea>
-      <Button
-        variant="primary"
-        type="submit"
-        style={{
-          width: "30%",
-          margin: "0",
-        }}
-      >
-        댓글 작성
-      </Button>
-      <Modal
-        show={isLoggedInModal}
-        onHide={handleCloseLoginModal}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "absolute",
-          top: "0",
-          left: "0",
-        }}
-      >
-        <ModalHeader closeButton>
-          <ModalTitle>로그인</ModalTitle>
-        </ModalHeader>
-        <ModalBody></ModalBody>
-        <ModalFooter>회원가입</ModalFooter>
-      </Modal>
-    </form>
+    <div>
+      <div className="comment-header">
+        <span className="writer">{comment.writer}</span>
+        <span className="upload-date">{formatTimeAgo(comment.write_date)}</span>
+      </div>
+      <p className="comment-content">{comment.comment}</p>
+      <div className="reply-container">
+        {comment.childCount > 0 ? (
+          <span
+            className="reply-list"
+            onClick={() => handleShowChild(comment.id)}
+          >
+            답글보기({comment.childCount})
+          </span>
+        ) : null}
+        <span className="reply-btn" onClick={() => setShowReplyForm(true)}>
+          답글
+        </span>
+      </div>
+      {showReply ? (
+        <div className="comment-list-container">
+          <ul className="comment-list">
+            {child.map((comment) => (
+              <li key={comment.id} className="comment-item">
+                <div className="comment-header">
+                  <span className="writer">{comment.writer}</span>
+                  <span className="upload-date">
+                    {formatTimeAgo(comment.write_date)}
+                  </span>
+                </div>
+                <p className="comment-content">{comment.comment}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
+      {showReplyForm ? (
+        <div>
+          <hr></hr>
+          <Form>
+            <InputGroup>
+              <InputGroup.Text>답글</InputGroup.Text>
+              <Form.Control
+                type="text"
+                name="comment"
+                value={reply.comment}
+                onChange={handleOnChange}
+                placeholder="댓글을 입력하세요."
+              />
+              <Button type="submit" onClick={handleOnSubmit}>
+                작성
+              </Button>
+              <Button type="button" onClick={() => setShowReplyForm(false)}>
+                취소
+              </Button>
+            </InputGroup>
+          </Form>
+        </div>
+      ) : null}
+    </div>
   );
 };
