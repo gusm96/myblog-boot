766f6ba94fab88ba3c165570a5a5a407d0fe10a9
Seongmo Gu <gusm96@naver.com>
2024-01-06 23:56:48 +0900

    CategoryAPI 메서드명 수정

--- src/main/java/com/moya/myblogboot/controller/CategoryController.java
+++ src/main/java/com/moya/myblogboot/controller/CategoryController.java
@@ -20,35 +20,36 @@ public class CategoryController {
     // 카테고리 리스트
     @GetMapping("/api/v1/categories")
     public ResponseEntity<List> getCategoryList() {
-        return ResponseEntity.ok(categoryService.getCategoryList());
+        return ResponseEntity.ok(categoryService.retrieveAll());
     }
 
     // 카테고리 리스트 V2
     @GetMapping("/api/v2/categories")
     public ResponseEntity<List> getCategoryListV2() {
-        return ResponseEntity.ok().body(categoryService.retrieveCategoriesDtoWithViewBoards());
+        return ResponseEntity.ok().body(categoryService.retrieveAllWithViewBoards());
     }
+
     // 관리자용 카테고리 리스트
     @GetMapping("/api/v1/categories-management")
-    public ResponseEntity<List> getCategoryListForAdmin (){
-        return ResponseEntity.ok().body(categoryService.retrieveCategoriesDto());
+    public ResponseEntity<List> getCategoryListForAdmin() {
+        return ResponseEntity.ok().body(categoryService.retrieveDto());
     }
 
     // 카테고리 작성
     @PostMapping("/api/v1/categories")
     public ResponseEntity<String> newCategory(@RequestBody @Valid CategoryReqDto categoryReqDto) {
-        return ResponseEntity.ok(categoryService.createCategory(categoryReqDto.getCategoryName()));
+        return ResponseEntity.ok(categoryService.create(categoryReqDto.getCategoryName()));
     }
 
     // 카테고리 수정
     @PutMapping("/api/v1/categories/{categoryId}")
-    public ResponseEntity<String> editCategory(@PathVariable Long categoryId, @RequestBody @Valid CategoryReqDto categoryReqDto ) {
-        return ResponseEntity.ok().body(categoryService.updateCategory(categoryId, categoryReqDto.getCategoryName()));
+    public ResponseEntity<String> editCategory(@PathVariable Long categoryId, @RequestBody @Valid CategoryReqDto categoryReqDto) {
+        return ResponseEntity.ok().body(categoryService.update(categoryId, categoryReqDto.getCategoryName()));
     }
 
     // 카테고리 삭제
     @DeleteMapping("/api/v1/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
-        return ResponseEntity.ok(categoryService.deleteCategory(categoryId));
+        return ResponseEntity.ok(categoryService.delete(categoryId));
     }
 }
