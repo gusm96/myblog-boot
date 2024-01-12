984a1e4f1c9a0d3df96a05360844d67068b29844
Seongmo Gu <gusm96@naver.com>
2024-01-13 00:30:46 +0900

    S3 FileUploadAPI 리팩터링

--- src/main/java/com/moya/myblogboot/controller/FileUploadController.java
+++ src/main/java/com/moya/myblogboot/controller/FileUploadController.java
@@ -17,14 +17,14 @@ public class FileUploadController {
 
     // S3 이미지 파일 업로드
     @PostMapping("/api/v1/images")
-    public ResponseEntity<ImageFileDto> requestUploadImageFile(@RequestParam("image") MultipartFile file) {
-        return ResponseEntity.ok().body(fileUploadService.saveImageFile(file));
+    public ResponseEntity<ImageFileDto> uploadImageFile(@RequestParam("image") MultipartFile file) {
+        return ResponseEntity.ok().body(fileUploadService.upload(file));
     }
 
     // S3 이미지 파일 삭제
     @DeleteMapping("/api/v1/images")
-    public ResponseEntity<?> requestDeleteImageFile(@RequestBody @Valid ImageFileDto imageFileDto) {
-        fileUploadService.deleteImageFile(imageFileDto.getFileName());
+    public ResponseEntity<?> deleteImageFile(@RequestBody @Valid ImageFileDto imageFileDto) {
+        fileUploadService.delete(imageFileDto.getFileName());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
