c23a18ab4617d12e737d22585df7a4e85c8247c2
Seongmo Gu <gusm96@naver.com>
2026-03-09 14:33:56 +0900

    refactor: Testcontainers 제거 및 로컬 Redis 기반 테스트 전환
    - AbstractContainerBaseTest에서 Testcontainers Redis 컨테이너 제거
    - TestcontainersConnectivityTest 삭제 (Docker Desktop 비호환으로 불필요)
    - application-test.yaml: H2 create-drop 모드 전환, sql init mode never

--- src/test/java/com/moya/myblogboot/AbstractContainerBaseTest.java
+++ src/test/java/com/moya/myblogboot/AbstractContainerBaseTest.java
@@ -1,25 +1,9 @@
 package com.moya.myblogboot;
 
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-
+/**
+ * 통합 테스트 기반 클래스.
+ * Redis는 application-test.yaml에 설정된 localhost:6379 (DB 1)를 사용한다.
+ * Docker/Testcontainers 없이 로컬 Redis 인스턴스에 의존한다.
+ */
 public abstract class AbstractContainerBaseTest {
-    static final String REDIS_IMAGE = "redis:6.0.16";
-    static final GenericContainer REDIS_CONTAINER;
-
-    static {
-        REDIS_CONTAINER = new GenericContainer<>(REDIS_IMAGE)
-                .withExposedPorts(6379)
-                .waitingFor(Wait.forListeningPort())
-                .withReuse(true);
-        REDIS_CONTAINER.start();
-    }
-
-    @DynamicPropertySource
-    public static void overrideProps(DynamicPropertyRegistry registry){
-        registry.add("spring.redis.host", REDIS_CONTAINER::getHost);
-        registry.add("spring.redis.port", () -> ""+REDIS_CONTAINER.getMappedPort(6379));
-    }
 }
